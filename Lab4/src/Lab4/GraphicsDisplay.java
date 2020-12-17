cd12656a510e2ad741038eafd7fd4fca2fb85a98
Manager <bulynko.sergey.y@gmail.com>
2020-12-17 22:13:25 +0300

    Ранние версии классов MainFrame и GraphicsDisplay

---
+++ Lab4/src/Lab4/GraphicsDisplay.java
@@ -0,0 +1,39 @@
+package Lab4;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Paint;
+import java.awt.Stroke;
+import java.awt.font.FontRenderContext;
+import java.awt.geom.GeneralPath;
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+import javax.swing.JPanel;
+import java.awt.GradientPaint;
+
+public class GraphicsDisplay extends JPanel
+{
+    // Список координат точек для построения графика
+    private Double[][] graphicsData;
+    // Флаговые переменные, задающие правила отображения графика
+    private boolean showAxis = true;
+    private boolean showMarkers = true;
+    private boolean showDivisions = true;
+    // Границы диапазона пространства, подлежащего отображению
+    private double minX;
+    private double maxX;
+    private double minY;
+    private double maxY;
+    // Используемый масштаб отображения
+    private double scale;
+    // Различные стили черчения линий
+    private BasicStroke graphicsStroke;
+    private BasicStroke axisStroke;
+    private BasicStroke markerStroke;
+    // Различные шрифты отображения надписей
+    private Font axisFont;
+}
---
+++ Lab4/src/Lab4/MainFrame.java
@@ -0,0 +1,36 @@
+package Lab4;
+
+import java.awt.BorderLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JOptionPane;
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuListener;
+
+@SuppressWarnings("serial")
+public class MainFrame extends JFrame {
+
+    // Начальные размеры окна приложения
+    private static final int WIDTH = 1000;
+    private static final int HEIGHT = 1000;
+    // Объект диалогового окна для выбора файлов
+    private JFileChooser fileChooser = null;
+    // Пункты меню
+    private JCheckBoxMenuItem showAxisMenuItem;
+    private JCheckBoxMenuItem showMarkersMenuItem;
+    // Компонент-отображатель графика
+    // Флаг, указывающий на загруженность данных графика
+    private boolean fileLoaded = false;
+}
